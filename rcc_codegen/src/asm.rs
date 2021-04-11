3244c171fc112c79a279f653848a4e1b6894a26e
Taiki Harada <haradataiki19990927@gmail.com>
2021-04-11 02:22:57 +0000

    Reg構造体の追加

---
+++ rcc_codegen/src/asm.rs
@@ -0,0 +1,19 @@
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Reg {
+    RAX,
+    RDI,
+    RSI,
+    RDX,
+    RCX,
+    RBP,
+    RSP,
+    RBX,
+    R8,
+    R9,
+    R10,
+    R11,
+    R12,
+    R13,
+    R14,
+    R15,
+}
