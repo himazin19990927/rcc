48e9a958fa5981ada91e734b58d3eaab599e1287
Taiki Harada <haradataiki19990927@gmail.com>
2021-04-16 11:23:26 +0000

    main.rsの修正

--- rcc_parser/src/main.rs
+++ rcc_parser/src/main.rs
@@ -1,10 +1,10 @@
 use rcc_parser::{lexer::Lexer, parser::Parser};
-use std::env;
+use std::env::{self, args};
 
 fn main() {
-    // let mut chars = "int a".chars();
-    // let res = read_keyword(&mut chars, "int");
-    // println!("result = {:?}, chars = {:?}", res, chars);
+    let src = args().nth(1).unwrap();
+    let stmt = Parser::new(Lexer::new(src.as_str())).stmt();
+    println!("{:?}", stmt);
 }
 
 #[allow(dead_code)]
@@ -14,24 +14,3 @@ fn parse_expr() {
     let expr = Parser::new(Lexer::new(src.as_str())).expr();
     println!("{:?}", expr);
 }
-
-// #[allow(dead_code)]
-// fn read_keyword(mut src: &Chars, keyword: &str) -> Option<String> {
-//     if keyword.len() > src.as_str().len() {
-//         return None;
-//     }
-
-//     let (first, last) = src.as_str().split_at(keyword.len());
-
-//     match last.chars().next() {
-//         Some(c) => {
-//             if !c.is_alphanumeric() && c != '_' {
-//                 return None;
-//             }
-//         }
-//         None => return None,
-//     }
-
-//     let _ = src.take(keyword.chars().count());
-//     return Some(keyword.to_string());
-// }
