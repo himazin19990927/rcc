3bbe7d86e547bec6a0ad6556aaa49425aac8c0c8
Taiki Harada <haradataiki19990927@gmail.com>
2021-05-01 06:15:56 +0000

    パーサーをlalrpopに変更

--- rcc_parser/src/lib.rs
+++ rcc_parser/src/lib.rs
@@ -1,4 +1,29 @@
 pub mod ast;
-pub mod lexer;
-pub mod parser;
-pub mod token;
+
+use ast::*;
+
+use lalrpop_util::lalrpop_mod;
+
+lalrpop_mod!(pub grammar);
+
+pub fn parse_expr(input: &str) -> Expr {
+    grammar::ExprParser::new().parse(input).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::parse_expr;
+    use crate::ast::*;
+
+    fn test_expr(src: &str, expect: Expr) {
+        assert_eq!(expect, parse_expr(src));
+    }
+
+    #[test]
+    fn parse_num() {
+        test_expr("0", Expr::Integer(0));
+        test_expr("10", Expr::Integer(10));
+        test_expr("01", Expr::Integer(1));
+        test_expr("(1)", Expr::Integer(1));
+    }
+}
--- rcc_parser/src/main.rs
+++ rcc_parser/src/main.rs
@@ -1,16 +1,16 @@
-use rcc_parser::{lexer::Lexer, parser::Parser};
-use std::env::{self, args};
+// use rcc_parser::{lexer::Lexer, parser::Parser};
+// use std::env::{self, args};
 
 fn main() {
-    let src = args().nth(1).unwrap();
-    let stmt = Parser::new(Lexer::new(src.as_str())).stmt();
-    println!("{:?}", stmt);
+    // let src = args().nth(1).unwrap();
+    // let stmt = Parser::new(Lexer::new(src.as_str())).stmt();
+    // println!("{:?}", stmt);
 }
 
-#[allow(dead_code)]
-fn parse_expr() {
-    let args: Vec<_> = env::args().collect();
-    let src = &args[1];
-    let expr = Parser::new(Lexer::new(src.as_str())).expr();
-    println!("{:?}", expr);
-}
+// #[allow(dead_code)]
+// fn parse_expr() {
+//     let args: Vec<_> = env::args().collect();
+//     let src = &args[1];
+//     let expr = Parser::new(Lexer::new(src.as_str())).expr();
+//     println!("{:?}", expr);
+// }
