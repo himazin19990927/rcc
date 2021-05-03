4a54e58b6d7fe01c766fe605fcfc40f4cefef230
Taiki Harada <haradataiki19990927@gmail.com>
2021-05-03 07:31:04 +0000

    TypeSpecifierを追加

--- rcc_parser/src/ast.rs
+++ rcc_parser/src/ast.rs
@@ -23,6 +23,13 @@ pub enum UnOp {
     Deref,
 }
 
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum TypeSpecifier {
+    Void,
+    Char,
+    Int,
+}
+
 #[derive(Debug, PartialEq)]
 pub enum Stmt {
     Print(Expr),
